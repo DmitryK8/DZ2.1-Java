5f950891bf93e0d338db0009c136d6050db25592
DMITRY KRAVCHENKO <88271958+DmitryK8@users.noreply.github.com>
2021-10-06 15:28:32 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int TicketPrice = 32_600;
+        int ticketPrice = 32_600;
         int costOfAMile = 20;
-        int BonusMile = TicketPrice / costOfAMile;
-        System.out.println(BonusMile + "Miles");
+        int bonusMile = ticketPrice / costOfAMile;
+        System.out.println(bonusMile + "miles");
     }
 }
